31fc17e9b1fdf0299003b3529b445623194e2292
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2022-09-02 16:41:29 +0200

    feat: add first layout

--- src/app/views/Home.js
+++ src/app/views/Home.js
@@ -1,8 +1,112 @@
-import { Image } from '@mantine/core';
-import React from 'react';
+import {
+  Container,
+  Grid,
+  Image,
+  NumberInput,
+  SegmentedControl,
+  Slider,
+  Space,
+  Text,
+  Title
+} from '@mantine/core';
+import React, { useEffect, useState } from 'react';
+import image from '../assets/mush.png';
 
-const Home = () => (
-  <Image src="https://c.tenor.com/5DRA1RGJ4ocAAAAC/start-gravityfalls.gif" fit="contain" />
-);
+const Home = () => {
+  const MUSH_BLOCK = 256;
+  const MUSH_BALL = 25;
+
+  const [weight, setWeight] = useState(0);
+  const [mush, setMush] = useState(0);
+  const [puppyPercentage, setPuppyPercentage] = useState(8);
+  const [lifeStage, setLifeStage] = useState('puppy');
+
+  useEffect(() => {
+    setMush((weight / 100) * (lifeStage === 'adult' ? 2 : puppyPercentage));
+  }, [weight, lifeStage, puppyPercentage]);
+
+  return (
+    <>
+      <Container>
+        <Image src={image} width="33%" fit="contain" />
+
+        <Space h="xl" />
+        <Space h="md" />
+
+        <SegmentedControl
+          value={lifeStage}
+          onChange={setLifeStage}
+          data={[
+            { label: 'Puppy', value: 'puppy' },
+            { label: 'Adult', value: 'adult' }
+          ]}
+        />
+      </Container>
+
+      <Space h="xl" />
+
+      <Container>
+        <NumberInput
+          value={weight}
+          decimalSeparator=","
+          onChange={value => setWeight(value * 1000)}
+          label="What does your dog weigh?"
+          hideControls
+          precision={1}
+        />
+      </Container>
+
+      <Space h="xl" />
+
+      <Container>
+        <Grid>
+          <Grid.Col span={6}>
+            <Title order={2}>Block(s)</Title>
+            <Text>{(mush / MUSH_BLOCK).toFixed(1)} / day</Text>
+            <Text size="sm" color="grey">
+              {mush}g{' '}
+              {lifeStage === 'puppy'
+                ? '(2-8 months 5-10% of body weight)'
+                : '(2% of desiered body weight)'}
+            </Text>
+          </Grid.Col>
+
+          <Grid.Col span={6}>
+            <Title order={2}>Ball(s)</Title>
+            <Text>{(mush / MUSH_BALL).toFixed(1)} / day</Text>
+            <Text size="sm" color="grey">
+              {mush}g{' '}
+              {lifeStage === 'puppy'
+                ? '(2-8 months 5-10% of body weight)'
+                : '(2% of desiered body weight)'}
+            </Text>
+          </Grid.Col>
+        </Grid>
+      </Container>
+
+      {lifeStage === 'puppy' && (
+        <Container>
+          <Space h="xl" />
+
+          <Text size="sm">Desired percentage of total weight</Text>
+          <Slider
+            min={5}
+            max={10}
+            value={puppyPercentage}
+            onChange={setPuppyPercentage}
+            marks={[
+              { value: 5, label: '5%' },
+              { value: 6, label: '6%' },
+              { value: 7, label: '7%' },
+              { value: 8, label: '8%' },
+              { value: 9, label: '9%' },
+              { value: 10, label: '10%' }
+            ]}
+          />
+        </Container>
+      )}
+    </>
+  );
+};
 
 export default Home;
